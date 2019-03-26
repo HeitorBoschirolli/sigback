86c6a17bf96042d63987332e5b80fd24a5e2631d
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2019-03-26 17:46:25 -0300

    test image processing

--- sigback/processing/tests/transform.py
+++ sigback/processing/tests/transform.py
@@ -1,10 +1,34 @@
 import numpy as np
 import unittest
+from skimage.io import imread
+from skimage import img_as_float64
+
 from sigback.processing import transform  
 
-def remove_border():
-    pass
+
+class ProcessingTest(unittest.TestCase):
+    test_data_path = './sigback/processing/tests/data'
+
+    def test_remove_border(self):
+        sig = img_as_float64(
+            imread(self.test_data_path + '/test_sig_no_bg.png')
+        )
+        actual = transform.remove_border(sig)
+        expected = img_as_float64(
+            imread(self.test_data_path + '/test_sig_cropped.png')
+        )
+        np.testing.assert_almost_equal(expected, actual, 0.01)
 
 
-def barycenter():
-    transform.barycenter(np.empty(5, 5))
+    def test_barycenter(self):
+        sig = img_as_float64(
+            imread(self.test_data_path + '/test_sig_cropped.png')
+        )
+        actual = transform.barycenter(sig)
+        expected = (192.0, 155.0)
+        self.assertAlmostEquals(actual[0], expected[0], 0.001)
+        self.assertAlmostEquals(actual[1], expected[1], 0.001)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- sigback/processing/transform.py
+++ sigback/processing/transform.py
@@ -10,20 +10,20 @@ def remove_border(img):
     pixels_on_row = np.sum(neg_img, axis=1)
     rows = np.nonzero(pixels_on_row)[0]
 
-    no_borders_img = img[rows[0]:rows[1]][cols[0]:cols[1]]
+    no_borders_img = img[rows[0]:rows[-1] + 1, cols[0]:cols[-1] + 1]
     return no_borders_img
 
 
 def barycenter(img):
     (num_rows, num_cols) = img.shape
 
-    bc_col = np.round(
+    bc_row = np.round(
         np.sum(
             np.array(range(num_cols)) * np.sum(img, axis=0),
             axis=0
         ) / np.sum(img)
     )
-    bc_row = np.round(
+    bc_col = np.round(
         np.sum(
             np.array(range(num_rows)) * np.sum(img, axis=1),
             axis=0
