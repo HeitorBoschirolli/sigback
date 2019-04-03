041b860a8cf066f8d9d3ed72633254099e28e4ab
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2019-04-03 18:08:45 -0300

    Adjust blend function
    Adapt blend function to blend signatures bigger than documents.
    Add tests for blending signatures bigger than documents individually and
    in directories.

--- sigback/blend/blend.py
+++ sigback/blend/blend.py
@@ -2,21 +2,29 @@ import numpy as np
 import random
 import os
 from skimage import img_as_float64
+from skimage.transform import rescale
 from skimage.io import imread, imsave
 
 from sigback.processing import color, transform
 
 
 def blend(sig, sig_barycenter, doc, doc_center):
+    if (sig.shape[0] > doc.shape[0]) or (sig.shape[1] > doc.shape[1]):
+        factor = min(
+            float(doc.shape[0]) / sig.shape[0],
+            float(doc.shape[1]) / sig.shape[1]
+        )
+        sig = rescale(sig, round(factor, 3) - 0.001)
+
     n_rows_doc, n_cols_doc = doc.shape
     n_rows_sig, n_cols_sig = sig.shape
 
     sig_start_row = int(np.ceil(doc_center[0] - sig_barycenter[0]))
     sig_start_col = int(np.ceil(doc_center[1] - sig_barycenter[1]))
 
-    if sig_start_row + n_rows_sig > n_rows_doc:
+    if (sig_start_row + n_rows_sig > n_rows_doc) or (sig_start_row < 0):
         sig_start_row = n_rows_doc - n_rows_sig
-    if sig_start_col + n_cols_sig > n_cols_doc:
+    if (sig_start_col + n_cols_sig > n_cols_doc) or (sig_start_col < 0):
         sig_start_col = n_cols_doc - n_cols_sig
     
     sig_end_row = sig_start_row + n_rows_sig
@@ -63,20 +71,7 @@ def blend_dirs(sigs_dir, docs_dir, doc_centers, save_dir, seed=None):
         sig = color.remove_background(sig)
         sig = transform.remove_border(sig)
         sig_barycenter = transform.barycenter(sig)
-
-        try:
-            blended = blend(
-                sig,
-                sig_barycenter,
-                doc,
-                doc_centers[index % n_docs]
-            )
-        except Exception as inst:
-            if sig.shape[0] > doc.shape[0] or sig.shape[1] > doc.shape[1]:
-                raise Exception(
-                    'The signature must be smaller than the document'
-                ) 
-            raise inst
+        blended = blend(sig, sig_barycenter, doc, doc_centers[index % n_docs])
 
         sig_name = os.path.split(sig_file)[-1]
         blended_file = os.path.join(save_dir, sig_name)
